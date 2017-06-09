f93681bee6f62c3c2b918c52475cde3465853d16
Allan Lukwago <epicallan.al@gmail.com>
2017-06-09 19:30:30 +0300

    gql to ts enhancements and corrections

--- src/schema/example/modules/Person/model.ts
+++ src/schema/example/modules/Person/model.ts
@@ -1,30 +1,26 @@
-const persons: DHschema.IPersonType[] = [
+const persons: DH.IPersonType[] = [
   {
-    __typename: 'person',
     id: '1',
     sex: 'male',
     name: 'miro'
   },
   {
-     __typename: 'person',
     id: '2',
     sex: 'female',
     name: 'lala'
   },
   {
-     __typename: 'person',
     id: '3',
     sex: 'male',
     name: 'joe'
   }
 ];
 
-
-const findPerson = (personsArr: DHschema.IPersonType[], id: string): DHschema.IPersonType => {
+const findPerson = (personsArr: DH.IPersonType[], id: string): DH.IPersonType => {
   return personsArr.find((person) => person.id === id);
 };
 
-const addPerson = (personsArr: DHschema.IPersonType[], person: DHschema.IPersonType) => {
+const addPerson = (personsArr: DH.IPersonType[], person: DH.IPersonType) => {
   personsArr.push(person);
   return personsArr;
 };
