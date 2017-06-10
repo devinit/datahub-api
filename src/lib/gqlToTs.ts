ef947b204c859966e306271674b359cfcd7aeed5
Allan Lukwago <epicallan.al@gmail.com>
2017-06-10 11:52:02 +0300

    separate build for lib folder for cli function & refactor

--- src/lib/gqlToTs.ts
+++
@@ -1,3 +0,0 @@
-import { generateTsFromGql, getTypeDefs} from './makeSchema';
-
-generateTsFromGql({outFile: './typings/global.d.ts', globPattern: '**/schema/*.gql'});
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,15 +2,19 @@
 import { GraphQLSchema} from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 import { mergeResolvers } from 'merge-graphql-schemas';
-import { getTypeDefs } from '../lib/makeSchema';
+import { getTypeDefs } from '../lib/makeTypeDefs';
+
 const resolverFiles = (require as any).context('./', true, /resolver\.ts/);
 
+// get resolver objects
 const resolversLoad: any[] = resolverFiles.keys()
     .map(moduleName => resolverFiles(moduleName).default);
 
+const resolvers = resolversLoad.length > 1
+    ? mergeResolvers(resolversLoad) : resolversLoad[0];
+
 const createSchema = async (): Promise<GraphQLSchema> => {
     const typeDefs = await getTypeDefs();
-    const resolvers = mergeResolvers(resolversLoad);
     return makeExecutableSchema({ typeDefs, resolvers });
 };
 
