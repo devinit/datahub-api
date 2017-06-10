c379248595fd6a307d752acf9014d240d04a54e5
Allan Lukwago <epicallan.al@gmail.com>
2017-06-10 15:31:46 +0300

    rename global.d.ts & minor changes

--- src/lib/cli/gqlToTs.js
+++ src/lib/cli/gqlToTs.js
@@ -1,7 +1,7 @@
 const makeTypeDefs = require('../dist/makeTypeDefs');
 
 try {
-    makeTypeDefs.generateTsFromGql({outFile: 'src/types/global.d.ts'});
+    makeTypeDefs.generateTsFromGql({outFile: 'src/types/dh.d.ts'});
 } catch(error) {
     console.log(error);
 }
