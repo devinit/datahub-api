5e71372cf879b09e80a37a241a3bba2eaa0957d6
Allan Lukwago <epicallan.al@gmail.com>
2017-06-11 10:39:57 +0300

    auto create context

---
+++ src/schema/example/db.ts
@@ -0,0 +1,22 @@
+// tslint:disable-next-line:no-reference
+// for vscode
+/// <reference path="../../types/dh.d.ts" />
+const persons: DH.IPersonType[] = [
+  {
+    id: '1',
+    sex: 'male',
+    name: 'miro'
+  },
+  {
+    id: '2',
+    sex: 'female',
+    name: 'lala'
+  },
+  {
+    id: '3',
+    sex: 'male',
+    name: 'joe'
+  }
+];
+
+export default persons;
