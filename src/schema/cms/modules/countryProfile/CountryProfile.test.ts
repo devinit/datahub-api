eac6ed07aee9efa8a264b17b6dfab1571f494a9b
Allan Lukwago <epicallan.al@gmail.com>
2017-06-18 13:17:01 +0300

    minor changes country profile test

--- src/schema/cms/modules/countryProfile/CountryProfile.test.ts
+++ src/schema/cms/modules/countryProfile/CountryProfile.test.ts
@@ -1,7 +1,7 @@
 import {getCountryProfilePageData} from '.';
 import * as prettyFormat from 'pretty-format';
 
-describe('Country profile module tests', () => {
+describe('country profile module tests', () => {
     it('should return page data for a country slug', async () => {
         const pageData: DH.IPage[] = await getCountryProfilePageData('usa');
         expect(pageData.length).toBeGreaterThan(2);
