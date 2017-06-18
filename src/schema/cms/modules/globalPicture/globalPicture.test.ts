a63c8f93e69c0c3c53ffe002a7f27fbac4d7db6f
Allan Lukwago <epicallan.al@gmail.com>
2017-06-18 13:03:43 +0300

    tests update

--- src/schema/cms/modules/globalPicture/globalPicture.test.ts
+++ src/schema/cms/modules/globalPicture/globalPicture.test.ts
@@ -1,8 +1,8 @@
-import {getThemes, getTheme} from '.';
+import {getGlobalPictureThemes, getTheme} from '.';
 
 describe('Site global Picture narratives and refrence module', () => {
     it('should return all themes', async () => {
-        const themes = await getThemes();
+        const themes = await getGlobalPictureThemes();
         const theme = getTheme('poorest20pct', themes);
         expect(themes[0].id).toBeDefined();
         expect(themes.length).toBeGreaterThan(4);
