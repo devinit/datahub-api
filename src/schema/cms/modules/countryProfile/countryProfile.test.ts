e4245bf73498b128252fc08cc7ce6df711eb0763
Allan Lukwago <epicallan.al@gmail.com>
2017-06-19 10:27:12 +0300

    merge with page-data work

--- src/schema/cms/modules/countryProfile/countryProfile.test.ts
+++ src/schema/cms/modules/countryProfile/countryProfile.test.ts
@@ -3,7 +3,7 @@ import * as prettyFormat from 'pretty-format';
 
 describe('country profile module tests', () => {
     // TOFIX: skipped coz it fails over network request issue
-    it.skip('should return page data for a country slug', async () => {
+    it('should return page data for a country slug', async () => {
         const pageData: DH.IPage[] = await getCountryProfilePageData('usa');
         expect(pageData.length).toBeGreaterThan(2);
         expect(prettyFormat(pageData)).toMatchSnapshot();
