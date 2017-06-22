5d5d49aeb31f6d2e94d440d32318067283ec866b
Allan Lukwago <epicallan.al@gmail.com>
2017-06-22 16:30:29 +0300

    country profiles wip#2

---
+++ src/schema/dw/modules/CountryProfile/CountryProfile.test.ts
@@ -0,0 +1,14 @@
+import * as prettyFormat from 'pretty-format';
+import CountryProfile from '.';
+import db from '../../db';
+
+describe('country profile DW module tests', () => {
+    const countryProfile = new CountryProfile(db);
+    it('should return overview tab data for uganda', async () => {
+        const overViewTab: DH.IOverViewTab = await countryProfile.getOverViewTab({id: 'UG'});
+        expect(prettyFormat(overViewTab)).toMatchSnapshot();
+    }, 50000);
+    afterAll(() => {
+       db.$config.pgp.end();
+    });
+});
