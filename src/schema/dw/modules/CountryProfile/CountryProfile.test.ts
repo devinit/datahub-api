e70fa70ad3a37448cb27cb2480ead5fe4ff03de0
Allan Lukwago <epicallan.al@gmail.com>
2017-06-25 10:20:36 +0300

    re-added tests

--- src/schema/dw/modules/CountryProfile/CountryProfile.test.ts
+++ src/schema/dw/modules/CountryProfile/CountryProfile.test.ts
@@ -3,16 +3,26 @@ import CountryProfile from '.';
 import db from '../../db';
 
 describe('country profile DW module tests', () => {
-    const countryProfile = new CountryProfile(db);
-    const overview = countryProfile.overViewTab;
+    let overview = null;
+
+    beforeAll(() => {
+        const countryProfile = new CountryProfile(db);
+        overview = countryProfile.overViewTab;
+        // waiting for DB to connect, timer
+        return setTimeout(() => {
+            return overview;
+        }, 1000);
+    });
+
     it('should return overview tab data for uganda', async () => {
         const overViewTab = await overview.getOverViewTab({id: 'UG'});
         expect(prettyFormat(overViewTab)).toMatchSnapshot();
-    }, 50000);
+    }, 10000);
     it('should return overview tab data for Austria', async () => {
         const overViewTab = await overview.getOverViewTab({id: 'AT'});
         expect(prettyFormat(overViewTab)).toMatchSnapshot();
-    }, 50000);
+    }, 10000);
+
     afterAll(() => {
        db.$config.pgp.end();
     });
