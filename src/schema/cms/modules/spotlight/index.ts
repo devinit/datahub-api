3e6d5b2591ae18f6feaee1f586a2962de3a14aee
Allan Lukwago <epicallan.al@gmail.com>
2017-09-18 19:51:26 +0300

    propagate district type changes

--- src/schema/cms/modules/spotlight/index.ts
+++ src/schema/cms/modules/spotlight/index.ts
@@ -5,16 +5,15 @@ import {get} from '../../connector';
 
 export interface IDistrict {
     id: string;
-    slug: string;
     name: string;
 }
 
 export const getDistrictEntities = (country: string): Promise<IDistrict[]> =>
-    get<IDistrict>(`spotlight-${country}/district-entity.csv`);
+    get<IDistrict>(`spotlight-${country}/district.csv`);
 
 export const getDistrictBySlugAsync = async (country: string, slug: string): Promise<IDistrict> => {
      const entities: IDistrict[] = await getDistrictEntities(country);
-     const district: IDistrict | undefined = entities.find(obj => obj.slug === slug);
+     const district: IDistrict | undefined = entities.find(obj => obj.name.toLowerCase() === slug);
      if (!district) throw new Error(`Error getting district entity for ${slug}`);
      return district;
 };
