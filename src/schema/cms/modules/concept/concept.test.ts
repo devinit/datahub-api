c4f8ff38a3c7a77ae5f0576b7162debcb97d9dfd
Allan Lukwago <epicallan.al@gmail.com>
2017-09-19 16:50:31 +0300

    new concepts tests

--- src/schema/cms/modules/concept/concept.test.ts
+++ src/schema/cms/modules/concept/concept.test.ts
@@ -5,10 +5,14 @@ import * as prettyFormat from 'pretty-format';
 describe('Github concept data tests', () => {
     it('should get data from concept.csv of a module on github', async () => {
         const conceptsData: IConcept[] = await getConcepts('global-picture');
+        const conceptsDataB: IConcept[] = await getConcepts('spotlight-uganda');
         expect(conceptsData.length).toBeGreaterThan(2);
+        expect(prettyFormat(conceptsDataB)).toMatchSnapshot();
     }, 50000);
     it('should get data from concept.csv of an Id in a module Async', async () => {
-        const concept: IConcept = await getConceptAsync('country-profile', 'data_series.domestic');
-        expect(prettyFormat(concept)).toMatchSnapshot();
+        const conceptA: IConcept = await getConceptAsync('country-profile', 'data_series.domestic');
+        const conceptB: IConcept =
+            await getConceptAsync('spotlight-uganda', 'spotlight_on_uganda.uganda_poverty_headcount');
+        expect(prettyFormat({conceptA, conceptB})).toMatchSnapshot();
     }, 50000);
 });
