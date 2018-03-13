2c4a5a9a5a37cd79b1be09ec4c465c16ad088659
epicallan <epicallan.al@gmail.com>
2018-03-13 10:25:43 +0300

    rename tools to scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+## gets run on host server
+mkdir -p ~/datahub-api
+cd ~/datahub-api || exit
+
+rm -rf datahub-api.git && \
+  git clone --depth 1 -b master --single-branch git@github.com:devinit/datahub-api.git datahub-api.git
+
+echo 'finished clonning'
+
+cd datahub-api.git || exit # go into application directory
+
+rm -rf datahub-api.git/.git # no need for git history
+
+echo 'rebuilding datahub-api docker containers'
+
+# make sure you added the db credentials to .bashrc or bash_profile as environment variables
+docker build --build-arg DB_USER=$DB_USER --build-arg DB_HOST=$DB_HOST --build-arg DB_PORT=$DB_PORT \
+    --build-arg DB_PASSWORD=$DB_PASSWORD --build-arg DB_DATABASE=$DB_DATABASE -t datahub-api .
+
+docker stop datahub-app-api
+
+docker rm datahub-app-api
+
+docker run -it -d -p 3000:3000 --name datahub-app-api datahub-api
+
+
+
+
+
