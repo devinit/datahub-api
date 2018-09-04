5bf6149193355ce47d67ef51e9593525a7243024
Edwin P. Magezi <edwinm_p@yahoo.com>
2018-09-04 11:08:17 +0300

    [Update] table names from _2015 to _constant

--- src/modules/dw/BubbleChart/sql.ts
+++ src/modules/dw/BubbleChart/sql.ts
@@ -4,8 +4,8 @@ export default {
     govtRevenuePerPerson: 'SELECT * FROM data_series.non_grant_revenue_ppp_pc WHERE year >= ${start_year} AND year <= ${end_year} ',
     percentageInExtremePoverty: 'SELECT di_id as id, year, value, di_id FROM data_series.poverty_190  WHERE value > 0 AND year >= ${start_year} AND year <= ${end_year} ',
     numberInExtremePoverty: 'SELECT di_id as id, year, value FROM data_series.poor_people_190 WHERE value > 0 AND year >= ${start_year} AND year <= ${end_year} ',
-    odaFrom: 'SELECT from_di_id FROM fact.oda_2015 WHERE value > 0 GROUP BY from_di_id',
-    odaTo: 'SELECT to_di_id From fact.\'oda_2015\' GROUP BY From_di_id',
-     // bubulesize data
+    odaFrom: 'SELECT from_di_id FROM fact.oda_constant WHERE value > 0 GROUP BY from_di_id',
+    odaTo: 'SELECT to_di_id From fact.\'oda_constant\' GROUP BY From_di_id',
+    // bubulesize data
     indicator: 'SELECT * FROM ${table^} WHERE year >= ${start_year} AND year <= ${end_year} AND value > 0'
 };
--- src/modules/dw/CountryProfile/sql.ts
+++ src/modules/dw/CountryProfile/sql.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 export default {
     population: 'SELECT value FROM fact.population_total WHERE di_id = ${id} AND year = ${start_year}',
-    // gdp: 'SELECT value FROM fact.gdp_usd_2015 WHERE di_id = ${id} AND year = ${start_year}',
+    // gdp: 'SELECT value FROM fact.gdp_usd_constant WHERE di_id = ${id} AND year = ${start_year}',
     domesticResourcesOverTime: 'SELECT * FROM data_series.domestic WHERE di_id =${id} AND l1 = ${l1} AND year >= ${start_year} AND value is NOT NULL',
     spendingAllocation: 'SELECT l2, value, budget_type FROM data_series.domestic WHERE di_id = ${id} AND year = ${end_year} AND l1 = \'total-expenditure\' AND l2 IS NOT NULL AND value > 0',
     // tslint:disable-next-line:max-line-length
@@ -14,7 +14,7 @@ export default {
     governmentSpendPerPerson: 'SELECT value FROM data_series.govt_spend_pc WHERE di_id = ${id} AND year <= ${start_year} AND budget_type=\'actual\' ANd value > 0 ORDER by year DESC',
     poorestPeople: 'SELECT value FROM data_series.poorest_20_percent WHERE di_id = ${id} AND year = ${start_year}',
     // tslint:disable-next-line:max-line-length
-    averageIncomerPerPerson: 'SELECT * FROM fact.gni_pc_usd_2015 WHERE di_id = ${id} AND year >= ${start_year} AND year <= ${end_year} AND value > 0',
+    averageIncomerPerPerson: 'SELECT * FROM fact.gni_pc_usd_constant WHERE di_id = ${id} AND year >= ${start_year} AND year <= ${end_year} AND value > 0',
     incomeDistTrend: 'SELECT value_bottom_20pc, value_2nd_quintile, value_3rd_quintile, value_4th_quintile, value_5th_quintile FROM fact.income_share_by_quintile WHERE di_id = ${id} AND value_bottom_20pc is NOT NULL ORDER BY year',
     // tslint:disable-next-line:max-line-length
     populationDistribution: 'SELECT * FROM fact.population_rural_urban WHERE di_id = ${id} AND year >= ${start_year} AND year <= ${end_year}',
@@ -22,8 +22,8 @@ export default {
     // tslint:disable-next-line:max-line-length
     poverty190Trend: 'SELECT di_id, value, year FROM data_series.poverty_190 WHERE di_id = ${id} AND year >= ${start_year} AND year <= ${end_year}',
     depthOfExtremePoverty: 'SELECT value FROM data_series.depth_of_extreme_poverty_190 WHERE di_id = ${id} AND year = ${start_year}',
-    GNI: 'SELECT value FROM fact.gni_usd_2015 WHERE di_id = ${id} AND year = ${start_year}',
-    ODANetIn: 'SELECT value FROM fact.in_oda_net_2015 WHERE di_id = ${id} AND year = ${start_year}', // computed
+    GNI: 'SELECT value FROM fact.gni_usd_constant WHERE di_id = ${id} AND year = ${start_year}',
+    ODANetIn: 'SELECT value FROM fact.in_oda_net_constant WHERE di_id = ${id} AND year = ${start_year}', // computed
     ODANetOut: 'SELECT value FROM fact.oda_percent_gni WHERE di_id = ${id} AND year = ${start_year}',
     // tslint:disable-next-line:max-line-length
     InflowsRecipient: 'SELECT sum(value) as value, flow_type, year FROM data_series.intl_flows_recipients WHERE di_id = ${id} AND year >= ${start_year} AND year <= ${end_year} AND value > 0 AND direction = \'in\' GROUP BY flow_type, year',
