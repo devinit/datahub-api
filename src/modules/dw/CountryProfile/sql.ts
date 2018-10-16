423be261018dea0b99b813f051b906367bce4d27
Edwin P. Magezi <edwinm_p@yahoo.com>
2018-10-16 16:37:11 +0300

    [Update] remove filtering for budget_type=actual

--- src/modules/dw/CountryProfile/sql.ts
+++ src/modules/dw/CountryProfile/sql.ts
@@ -5,10 +5,10 @@ export default {
     domesticResourcesOverTime: 'SELECT * FROM data_series.domestic WHERE di_id =${id} AND l1 = ${l1} AND year >= ${start_year} AND value is NOT NULL',
     spendingAllocation: 'SELECT l2, value, budget_type FROM data_series.domestic WHERE di_id = ${id} AND year = ${end_year} AND l1 = \'total-expenditure\' AND l2 IS NOT NULL AND value > 0',
     // tslint:disable-next-line:max-line-length
-    domesticRevenue: 'SELECT value FROM data_series.domestic WHERE di_id =${id} AND budget_type = \'actual\' AND year = ${end_year} AND l1 = \'total-revenue-and-grants\' AND l2 = \'revenue\' AND l3 is NULL AND l4 is NULL',
-    totalDomesticRevenueAndGrants: 'SELECT value FROM data_series.domestic WHERE di_id =${id} AND budget_type = \'actual\' AND year = ${end_year} AND l1 = \'total-revenue-and-grants\' AND l2 is NULL AND l3 is NULL AND l4 is NULL',
+    domesticRevenue: 'SELECT value FROM data_series.domestic WHERE di_id =${id} AND year = ${end_year} AND l1 = \'total-revenue-and-grants\' AND l2 = \'revenue\' AND l3 is NULL AND l4 is NULL',
+    totalDomesticRevenueAndGrants: 'SELECT value FROM data_series.domestic WHERE di_id =${id} AND year = ${end_year} AND l1 = \'total-revenue-and-grants\' AND l2 is NULL AND l3 is NULL AND l4 is NULL',
     // tslint:disable-next-line:max-line-length
-    grants: 'SELECT value FROM data_series.domestic WHERE di_id =${id} AND budget_type = \'actual\' AND year = ${end_year} AND l1 = \'total-revenue-and-grants\' AND l2 = \'grants\' AND l3 is NULL AND l4 is NULL',
+    grants: 'SELECT value FROM data_series.domestic WHERE di_id =${id} AND year = ${end_year} AND l1 = \'total-revenue-and-grants\' AND l2 = \'grants\' AND l3 is NULL AND l4 is NULL',
     // tslint:disable-next-line:max-line-length
     internationalResources: 'SELECT sum(value) as value FROM data_series.intl_flows_recipients WHERE di_id = ${id} AND direction =\'in\' AND year=${end_year} group by year',
     governmentSpendPerPerson: 'SELECT value FROM data_series.govt_spend_pc WHERE di_id = ${id} AND year <= ${start_year} AND budget_type=\'actual\' ANd value > 0 ORDER by year DESC',
